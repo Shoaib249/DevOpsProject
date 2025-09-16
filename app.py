c142a970d5104d764352866bdb3144985865bca9
Muhammad Shoaib Khan <Shoaibkhan100@outlook.com>
2025-09-16 19:47:04 +0000

    Auto-format Python code with Black

--- app.py
+++ app.py
@@ -5,63 +5,73 @@ import re
 import config
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = "your_secret_key"
 
-app.config['MYSQL_HOST'] = config.MYSQL_HOST
-app.config['MYSQL_USER'] = config.MYSQL_USER
-app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
-app.config['MYSQL_DB'] = config.MYSQL_DB
+app.config["MYSQL_HOST"] = config.MYSQL_HOST
+app.config["MYSQL_USER"] = config.MYSQL_USER
+app.config["MYSQL_PASSWORD"] = config.MYSQL_PASSWORD
+app.config["MYSQL_DB"] = config.MYSQL_DB
 
 mysql = MySQL(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return redirect(url_for('login'))
+    return redirect(url_for("login"))
 
-@app.route('/signup', methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    msg = ''
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        password = request.form['password']
+    msg = ""
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
 
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM users WHERE email = %s', (email,))
+        cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
         account = cursor.fetchone()
 
         if account:
-            msg = 'Account already exists!'
+            msg = "Account already exists!"
         else:
-            cursor.execute('INSERT INTO users (name, email, password) VALUES (%s, %s, %s)', (name, email, password))
+            cursor.execute(
+                "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
+                (name, email, password),
+            )
             mysql.connection.commit()
-            msg = 'You have successfully registered!'
-    return render_template('signup.html', msg=msg)
+            msg = "You have successfully registered!"
+    return render_template("signup.html", msg=msg)
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    msg = ''
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+    msg = ""
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
 
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM users WHERE email = %s AND password = %s', (email, password))
+        cursor.execute(
+            "SELECT * FROM users WHERE email = %s AND password = %s", (email, password)
+        )
         account = cursor.fetchone()
 
         if account:
-            session['loggedin'] = True
-            session['name'] = account[1]
-            return redirect(url_for('welcome'))
+            session["loggedin"] = True
+            session["name"] = account[1]
+            return redirect(url_for("welcome"))
         else:
-            msg = 'Incorrect email or password!'
-    return render_template('login.html', msg=msg)
+            msg = "Incorrect email or password!"
+    return render_template("login.html", msg=msg)
 
-@app.route('/welcome')
+
+@app.route("/welcome")
 def welcome():
-    if 'loggedin' in session:
-        return render_template('welcome.html', name=session['name'])
-    return redirect(url_for('login'))
+    if "loggedin" in session:
+        return render_template("welcome.html", name=session["name"])
+    return redirect(url_for("login"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
